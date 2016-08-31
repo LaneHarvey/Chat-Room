79c4ab24e78e169977b80c5c408a84698741bb46
Lane Harvey <mrlaneharvey@gmail.com>
2016-08-31 10:48:30 -0600

    first commit

--- js/messageService.js
+++ js/messageService.js
@@ -1,6 +1,7 @@
 angular.module('chatroom').service('messageService', function($http){
   //Here you'll need to create two methods. One called postMessage and the other called getMessages.
-
+var getMessage = function(){};
+var postMessage = function(){};
   //On the lines below create a getMessages method. This method will retrieve data from the backend.
   //The url for the get request should be 'http://practiceapi.devmounta.in/api/chats'
   //Be sure to return whatever gets returned from $http so you can call .then in your controller.
