f3f9a2654ae2576cc145f3664488fab52995475e
Lane Harvey <mrlaneharvey@gmail.com>
2016-08-31 10:40:41 -0600

    first commit

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -1,6 +1,7 @@
 angular.module('chatroom').controller('mainCtrl', function($scope, messageService){
   //In your controller you'll have a getMessages function and a postMessage function, but should be placed on $scope.
-
+$scope.getMessage = function(){};
+$scope.postMessage = function(){};
   //The getMessages function will call the getData method on the messageService object. You'll then save the result of that request to
   //your controllers $scope as messages ($scope.messages)
 
